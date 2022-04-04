f2ab97572c0cce73eefa9c1ccbe0f453a41a51bc
kushal <kushal_vaghani@yahoo.com>
2022-04-04 12:01:30 -0400

    missed to add itemlist save at sceneDidEnterBackground, which didnt save items, but is now working fine:)

--- Inventory/SceneDelegate.swift
+++ Inventory/SceneDelegate.swift
@@ -62,7 +62,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func sceneDidEnterBackground(_ scene: UIScene) {
      
-        
+        itemList.save()
         
         
         // Called as the scene transitions from the foreground to the background.
